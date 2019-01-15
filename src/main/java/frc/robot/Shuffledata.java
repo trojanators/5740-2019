9c857c66d1e3bf0d3e5349896939860bb44b3e8a
BuildTools <frc5740@gmail.com>
2019-01-15 10:48:54 -0500

    setup shuffleboard

--- src/main/java/frc/robot/Shuffledata.java
+++ src/main/java/frc/robot/Shuffledata.java
@@ -7,5 +7,6 @@ public class Shuffledata {
     public static void Periodic(){
         SmartDashboard.putNumber("Xbox X value", OI.controller1.getRawAxis(1));
         SmartDashboard.putNumber("xbox y value", OI.controller1.getRawAxis(4));
-}
+        SmartDashboard.putData("pressure-switch", OI.compressorSpike);
+    }
 }
