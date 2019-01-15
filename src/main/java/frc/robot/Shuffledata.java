2eec0a29d40bf6638d3e12be6446d953d46cc541
BuildTools <frc5740@gmail.com>
2019-01-15 12:05:55 -0500

    added encoder data to shuffle board dash

--- src/main/java/frc/robot/Shuffledata.java
+++ src/main/java/frc/robot/Shuffledata.java
@@ -10,7 +10,8 @@ public class Shuffledata {
         SmartDashboard.putNumber("Xbox X value", OI.controller1.getRawAxis(1));
         SmartDashboard.putNumber("xbox y value", OI.controller1.getRawAxis(4));
         SmartDashboard.putData("pressure-switch", OI.pressureSwitch);
-        
+        SmartDashboard.putData("Right Encoder", OI.rightDriveEncoder);
+        SmartDashboard.putData("Left Encoder", OI.leftDriveEncoder)
         
     }
 }
